6cc467558d56dc367ee23d09e3023072ed02a721
Dmitry Drepin <lixefer@gmail.com>
2023-09-04 17:19:11 +0200

    service discovery update

---
+++ ServiceDiscovery/src/main/java/org/dzmdre/DiscoveryServiceApplication.java
@@ -0,0 +1,17 @@
+package org.dzmdre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
