273007b67bad9de8dfbd6c15b04d6ba09e8a6ad0
Dmitry Drepin <lixefer@gmail.com>
2023-09-06 19:21:51 +0200

    change orders snapshotting & error handling

--- OrdersService/src/main/java/org/dzmdre/OrdersService/OrdersServiceApplication.java
+++ OrdersService/src/main/java/org/dzmdre/OrdersService/OrdersServiceApplication.java
@@ -1,15 +1,34 @@
 package org.dzmdre.OrdersService;
 
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class OrdersServiceApplication {
 
+	public static final String ORDER_GROUP = "order-group";
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrdersServiceApplication.class, args);
 	}
 
+	@Bean(name="orderSnapshotTriggerDefinition")
+	public SnapshotTriggerDefinition orderSnapshotTriggerDefinition(Snapshotter snapshotter) {
+		return new EventCountSnapshotTriggerDefinition(snapshotter, 3);
+	}
+
+	@Autowired
+	public void configure(EventProcessingConfigurer config) {
+		config.registerListenerInvocationErrorHandler(ORDER_GROUP,
+				conf -> PropagatingErrorHandler.instance());
+	}
 }
--- OrdersService/src/main/java/org/dzmdre/OrdersService/command/rest/OrdersCommandController.java
+++ OrdersService/src/main/java/org/dzmdre/OrdersService/command/rest/OrdersCommandController.java
@@ -1,11 +1,9 @@
 package org.dzmdre.OrdersService.command.rest;
 
 import jakarta.validation.Valid;
-import org.dzmdre.OrdersService.command.rest.CreateOrderCommand;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.dzmdre.OrdersService.core.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +16,9 @@ import java.util.UUID;
 @RequestMapping("/orders")
 public class OrdersCommandController {
     public static final String USER_ID_CONSTANT = "27b95829-4f3f-4ddf-8983-151ba010e35b";
-    private final Environment env;
     private final CommandGateway commandGateway;
     @Autowired
-    public OrdersCommandController(Environment env, CommandGateway commandGateway) {
-        this.env = env;
+    public OrdersCommandController(CommandGateway commandGateway) {
         this.commandGateway = commandGateway;
     }
     @PostMapping
